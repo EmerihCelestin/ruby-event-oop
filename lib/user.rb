42ce76b2687475568623db99d4f27c3895e6991f
emerihcelestin <emerih02@gmail.com>
2019-07-22 14:51:31 +0300

    user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,18 @@
-class User
+require 'pry'
 
+class User
+	attr_accessor :email, :age
+	@@all_user = []
+	def initialize(email_to_save,age_to_save)
+			@email = email_to_save
+			@age = age_to_save
+			@@all_user << self
+	end		
+
+	def self.all
+		puts @@all_user
+	end
 end
+
+binding.pry
+puts "end of file"
